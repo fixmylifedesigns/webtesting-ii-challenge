7700d8c40ccc97871f73f16fea04cd9305a86dbd
Irving Duran <ijd.irving@gmail.com>
2019-05-15 10:54:39 -0300

    mvp

---
+++ baseball/src/components/Display.js
@@ -0,0 +1,80 @@
+import React, { Component } from "react";
+import Dashboard from "./Dashboard";
+
+class Display extends Component {
+    state = {
+      balls: 0,
+      strikes: 0,
+    };
+  
+    hit = () => {
+      this.setState({
+        balls: 0,
+        strikes: 0
+      });
+    };
+  
+    strike = () => {
+      if (this.state.strikes < 2) {
+        this.setState({
+          ...this.state,
+          strikes: this.state.strikes + 1
+        });
+      } else {
+        this.setState({
+          strikes: 0
+        });
+      }
+    };
+  
+   ball = () => {
+      if (this.state.balls < 3) {
+        this.setState({
+          ...this.state,
+          balls: this.state.balls + 1
+        });
+      } else {
+        this.setState({
+          balls: 0,
+         
+        });
+      }
+    };
+
+    foul = () => {
+      if (this.state.strikes < 2) {
+        this.setState({
+          ...this.state,
+          strikes: this.state.strikes + 1
+        });
+      } else {
+        this.setState({
+          ...this.state
+        });
+      }
+    };
+  
+ 
+  
+    render() {
+      return (
+        <div>
+          <h1>Count</h1>
+  
+          <div>
+            <h2>Balls: {this.state.balls}</h2>
+            <h2>Strikes: {this.state.strikes}</h2>
+          </div>
+  
+          <Dashboard
+            hit={this.hit}
+            strike={this.strike}
+            foul={this.foul}
+            ball={this.ball}
+          />
+        </div>
+      );
+    }
+  }
+  
+  export default Display;
